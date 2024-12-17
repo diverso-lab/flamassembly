f9841aa41385b3b58f596369aab137c4e7571421
David Romero <drorganvidez@us.es>
2024-12-17 09:42:39 +0100

    feat: Improve flamapy.js

--- flamapy.js
+++ flamapy.js
@@ -1,24 +1,22 @@
 let pyodide;
 
-async function loadAndRunPythonScript(scriptPath, functionCall) {
+const loadAndRunPythonScript = async (scriptPath, functionCall) => {
     try {
         const response = await fetch(scriptPath);
         const pythonScript = await response.text();
-        await pyodide.runPythonAsync(pythonScript + `\n${functionCall}`);
+        await pyodide.runPythonAsync(`${pythonScript}\n${functionCall}`);
     } catch (err) {
         console.error(`Error ejecutando el script Python desde ${scriptPath}:`, err);
         throw err;
     }
-}
+};
 
-async function prepareWASM() {
+const prepareWASM = async () => {
     try {
-        // Cargar Pyodide y micropip
         pyodide = await loadPyodide();
         await pyodide.loadPackage("micropip");
         console.log("Micropip cargado.");
 
-        // Cargar y ejecutar el script de instalación de paquetes
         await loadAndRunPythonScript("packages.py", "await install_flamapy_packages()");
         console.log("Paquetes de Flamapy instalados.");
 
@@ -26,11 +24,10 @@ async function prepareWASM() {
     } catch (err) {
         console.error("Error preparando Pyodide y Flamapy:", err);
     }
-}
+};
 
-async function runFlamapyMethod(param) {
+const runFlamapyMethod = async (param) => {
     showLoading();
-
     try {
         await loadAndRunPythonScript("flamapy_methods.py", `run_flamapy_method("${param}")`);
     } catch (err) {
@@ -38,4 +35,4 @@ async function runFlamapyMethod(param) {
     } finally {
         hideLoading();
     }
-}
+};
